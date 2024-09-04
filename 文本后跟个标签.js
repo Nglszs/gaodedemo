ca1e65e2d4c3c9aac83f9903565730c2dda8b945
Jack <1458583930@qq.com>
2024-09-04 19:29:23 +0800

    Add files via upload

---
+++ 文本后跟个标签.js
@@ -0,0 +1,35 @@
+  <View>
+        <Text style={{
+          lineHeight: 20,
+          fontSize: 14,
+          marginTop: 60,
+          marginLeft: 20,
+          marginRight: 20,
+        }}>
+            这是一段较长的文本，可能会根据屏幕宽度换行显示。当文本达到一定长度并且需要换行时，
+            我们希望后面的标签（比如“详细”）能够跟随文本自动换到下一行 
+            <View style={{height: 15, backgroundColor: '#00B42B', borderRadius: 2, paddingHorizontal: 4}}>
+              <Text style={{ fontSize: 12, color: 'white', fontWeight: '600' }}>通过</Text>
+            </View>
+         
+        </Text>
+      </View>
+
+      <View>
+        <Text style={{
+          lineHeight: 20,
+          fontSize: 14,
+          marginTop: 60,
+         
+          marginLeft: 20,
+          marginRight: 20,
+         
+        }}>
+            这是一段较长的文本，可能会根据屏幕宽度换行显示
+            <View style={{height: 15, backgroundColor: '#1A6AFF', borderRadius: 2, paddingHorizontal: 4}}>
+              <Text style={{ fontSize: 12, color: 'white', fontWeight: '600' }}>拒绝</Text>
+            </View>
+         
+        </Text>
+      </View>
+
